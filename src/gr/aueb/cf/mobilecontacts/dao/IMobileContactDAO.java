2d7b13aacaaec95b3b950c3f139913c24c633372
Katerina Savidaki <ksavvidaki@gmail.com>
2025-01-27 19:02:40 +0200

    Initian Commit

---
+++ src/gr/aueb/cf/mobilecontacts/dao/IMobileContactDAO.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.mobilecontacts.dao;
+
+
+import gr.aueb.cf.mobilecontacts.model.MobileContact;
+
+import java.util.List;
+
+/**
+ * CRUD Public API
+ */
+public interface IMobileContactDAO {
+    MobileContact insert(MobileContact mobileContact);
+    MobileContact update(Long id, MobileContact mobileContact);
+    void deleteById(Long id);
+    MobileContact getById(Long id);
+    List<MobileContact> getAll();
+
+    void deleteByPhoneNumber(String phoneNumber);
+
+    MobileContact getByPhoneNumber(String phoneNumber);     //queries
+    boolean userIdExists(Long id);
+    boolean phoneNumberExists(String phoneNumber);
+}
