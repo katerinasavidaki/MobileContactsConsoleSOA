91583d09e534914ee5b7d29e5873133df2751b56
Katerina Savidaki <ksavvidaki@gmail.com>
2025-02-03 18:25:07 +0200

    final updates

--- src/gr/aueb/cf/mobilecontacts/Main.java
+++ src/gr/aueb/cf/mobilecontacts/Main.java
@@ -2,7 +2,9 @@ package gr.aueb.cf.mobilecontacts;
 
 import gr.aueb.cf.mobilecontacts.controller.MobileContactController;
 import gr.aueb.cf.mobilecontacts.dto.MobileContactInsertDTO;
+import gr.aueb.cf.mobilecontacts.dto.MobileContactUpdateDTO;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -51,19 +53,59 @@ public class Main {
                 }
                 break;
             case "2":
-                //
+                System.out.println("Please insert phone number");
+                phoneNumber = getToken();
+                response = controller.getContactByPhoneNumber(phoneNumber);
+                if (response.startsWith("Error")) {
+                    System.out.println("Contact was not found");
+                    System.out.println(response.substring(7));
+                    return;
+                }
+                System.out.println("Unsuccessful insertion");
+                System.out.println(response.substring(6));
+                System.out.println("Please insert the existing id");
+                long oldId = Long.parseLong(getToken());
+                System.out.println("Please insert new firstname, new lastname, new phone number");
+                firstname = getToken();
+                lastname = getToken();
+                phoneNumber = getToken();
+                MobileContactUpdateDTO updateDTO = new MobileContactUpdateDTO(oldId, firstname, lastname, phoneNumber);
+                response = controller.updateContact(updateDTO);
+                System.out.println(response);
                 break;
             case "3":
-                //
+                System.out.println("Insert Contact id");
+                long id = Long.parseLong(getToken());
+                response = controller.deleteContactById(id);
+                if (response.startsWith("OK")) {
+                    System.out.println("Successful deletion");
+                    System.out.println(response.substring(3));
+                } else {
+                    System.out.println("Unsuccessful deletion");
+                    System.out.println(response.substring(6));
+                }
                 break;
             case "4":
-                //
+                System.out.println("Insert Contact id");
+                id = Long.parseLong(getToken());
+                response = controller.getContactById(id);
+                if (response.startsWith("OK")) {
+                    System.out.println("Successful Search");
+                    System.out.println(response.substring(3));
+                } else {
+                    System.out.println("Search was unsuccessful");
+                    System.out.println(response.substring(6));
+                }
                 break;
             case "5":
-                //
+                List<String> mobileContacts = controller.getAllContacts();
+                if (mobileContacts.isEmpty()) {
+                    System.out.println("Empty contact list");
+                }
+                mobileContacts.forEach(System.out::println);
                 break;
             default:
-                //
+                System.out.println("Invalid choice");
                 break;
         }
     }
